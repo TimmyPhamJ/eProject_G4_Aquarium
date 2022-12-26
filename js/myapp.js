90bc85e5289b8a8cef8e8984dc23612573791fa6
PhanVinhThuan <pvthuan.work@gmail.com>
2022-12-26 21:15:29 +0700

    Thuan comit 26/12
    chua hoan thien dang taoj json k dua len main

--- js/myapp.js
+++ js/myapp.js
@@ -1,26 +1,90 @@
 var myApp =angular.module("myApp", [
     'ngRoute',
-    'homedefault',
-    'activities'
+    'home',
+    'active',
+    'event',
+    'contact',
+    'about',
+    'plan',
+    'menu'
 ]);
 
-myApp.config(function($routeProvider){
+myApp.config(["$routeProvider","$locationProvider",function($routeProvider){
     $routeProvider
     .when("/",{
-        templateUrl:"homedefault.html",controller:"homedefaultCtrl"
+        templateUrl:"home.html",controller:"homeCtrl"
     })
-    .when("/activities",{
-        templateUrl:"activities.html",controller:"activitiesCtrl"
+    .when("/active",{
+        templateUrl:"activities.html",controller:"activeCtrl"
     })
-})
-
-
-angular.module("homedefault",[])
-    .controller("homedefaultCtrl",['$scope','$http',function($scope,$http){
+    .when("/event",{
+        templateUrl:"events.html",controller:"eventCtrl"
+    })
+    .when("/contact",{
+        templateUrl:"contact.html",controller:"contactCtrl"
+    })
+    .when("/about",{
+        templateUrl:"about.html",controller:"aboutCtrl"
+    })
+    .when("/plan",{
+        templateUrl:"plan.html",controller:"planCtrl"
+    })
 
 }]);
 
-angular.module("activities",[])
-    .controller("activitiesCtrl",['$scope','$http',function($scope,$http){
+
+angular.module("home",[])
+    .controller("homeCtrl",['$scope','$http',function($scope,$http){
+        $http.get("../json/home.json")
+        .then(function(response){
+            $scope.HomeList = response.data;
+            console.log(response.data);
+        },
+        function(err){
+            $scope.HomeList = "Request JSON failed.";
+        });
 
 }]);
+
+angular.module("active",[])
+    .controller("activeCtrl",['$scope','$http',function($scope,$http){
+        $http.get("../json/activities.json")
+        .then(function(response){
+            $scope.ActivitiesList = response.data;
+            console.log(response.data);
+        },
+        function(err){
+            $scope.ActivitiesList = "Request JSON failed.";
+        });
+        
+
+}]);
+
+angular.module("event",[])
+    .controller("eventCtrl",['$scope','$http',function($scope,$http){
+
+}]);
+
+angular.module("contact",[])
+    .controller("contactCtrl",['$scope','$http',function($scope,$http){
+
+}]);
+
+angular.module("about",[])
+    .controller("aboutCtrl",['$scope','$http',function($scope,$http){
+
+}]);
+
+angular.module("plan",[])
+    .controller("planCtrl",['$scope','$http',function($scope,$http){
+
+}]);
+
+angular.module("menu",[])
+    .controller("menuCtrl",['$scope','$location','$route',function($scope,$location,$route){
+        $scope.activePath =null;
+        $scope.$on('$routeChangeSuccess',function(){
+            $scope.activePath = $location.path();
+            console.log($location.path());
+        });
+    }]);
